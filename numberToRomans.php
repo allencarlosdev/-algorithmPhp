7d76ed91ce6b64b04f5e42d5f55f7107a901ee76
Carlos Allen <allencarlosdev@gmail.com>
2023-01-13 18:59:46 -0600

    Convert the numbers to romans

---
+++ numberToRomans.php
@@ -0,0 +1,21 @@
+<?php
+
+echo 'convert the numbers to romans';
+
+function romanToInt($s) {
+    define('VALUES', array(1000,900,500,400,100,90,50,40,10,9,5,4,1));
+    define('ROMANS', array('M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'));
+    $result = '';
+    foreach (VALUES as $i => $value) {
+        while ($s >= $value) {
+            $result= $result.ROMANS[$i];
+            $s=$s - $value;
+        }
+    }
+
+    echo $result;
+}
+
+$s="24";
+romanToInt($s);
+?>
